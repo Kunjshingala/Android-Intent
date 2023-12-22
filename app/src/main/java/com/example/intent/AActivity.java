6cdeeb90f64041ca95fc6357cdcca151a2a99e70
Kunj Shingala <kunjshingala.p@gmail.com>
2023-12-22 13:21:09 +0530

    add data passing in activities through tost and textView

--- app/src/main/java/com/example/intent/AActivity.java
+++ app/src/main/java/com/example/intent/AActivity.java
@@ -2,25 +2,44 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AActivity extends AppCompatActivity {
     Button btn_one;
+    EditText input_email;
+    EditText input_country;
+    EditText input_enrollment;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aactivity);
 
         btn_one = (Button) findViewById(R.id.btn_one);
+
+        input_email = (EditText) findViewById(R.id.input_email);
+        input_country = (EditText) findViewById(R.id.input_country);
+        input_enrollment = (EditText) findViewById(R.id.input_enrollment);
         btn_one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                String email = input_email.getText().toString();
+                String country = input_country.getText().toString();
+                String enrollment = input_enrollment.getText().toString();
+
                 Intent intent = new Intent(getApplicationContext(), BActivity.class);
+                intent.putExtra("key_email", email);
+                intent.putExtra("key_country", country);
+                intent.putExtra("key_enrollment", enrollment);
+
+
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/intent/BActivity.java
+++ app/src/main/java/com/example/intent/BActivity.java
@@ -2,10 +2,13 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BActivity extends AppCompatActivity {
 
@@ -17,6 +20,24 @@ public class BActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bactivity);
 
         btn_two = (Button) findViewById(R.id.btn_two);
+
+        Bundle bundle = getIntent().getExtras();
+        String email = bundle.getString("key_email");
+        String country = bundle.getString("key_country");
+        String enrollment = bundle.getString("key_enrollment");
+
+//        to display data in tost
+//        Toast.makeText(this, email + " , " + country+" , "+enrollment,Toast.LENGTH_SHORT).show();
+
+        TextView txtView_email = findViewById(R.id.display_email);
+        txtView_email.setText("Email : " + email);
+
+        TextView txtView_counrty = findViewById(R.id.display_country);
+        txtView_counrty.setText("Coutry : " + country);
+
+        TextView txtView_enrollment = findViewById(R.id.display_enrollment);
+        txtView_enrollment.setText("Enrollment : " + enrollment);
+
 
         btn_two.setOnClickListener(new View.OnClickListener() {
             @Override
